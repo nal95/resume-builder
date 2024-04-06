64c99e18cb4a3f605f5ac08be4d6d3095480b709
nal95 <alex.nguepi@yahoo.fr>
2024-04-06 08:00:32 +0200

    add user data store service

---
+++ frontend/src/app/services/user-data-store/user-data-store.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from "rxjs";
+import {User} from "../../resume-data/user.data";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserDataStoreService {
+
+  private readonly _userData = new BehaviorSubject<User>({} as User);
+
+  readonly userData$ = this._userData.asObservable();
+
+  get userData(): User {
+    return this._userData.getValue();
+  }
+
+  private set userData(val: User) {
+    this._userData.next(val);
+  }
+
+  setUserData(val: User) {
+    this.userData = val;
+  }
+
+}
